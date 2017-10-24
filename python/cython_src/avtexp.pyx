aa7448fe664387474fed41439bc1cabc06ac7ce0
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2017-10-24 10:24:21 +0200

    Removed the Cython code before transition to pybind11

--- python/cython_src/avtexp.pyx
+++
@@ -1,5 +0,0 @@
-cimport c_avtexp
-
-def perform_experiment(sz, num_req, tau_sleep, tau_mf, verbose, do_save_images):
-
-    c_avtexp.launchImageStreamReadExperiment(sz, num_req, tau_sleep, tau_mf, verbose, do_save_images)
--- python/python_src/isr-experiment.py
+++
@@ -1,5 +0,0 @@
-import avtexp
-
-if __name__ == '__main__':
-
-    avtexp.perform_experiment(20, 15, 180, 50, True, False)
