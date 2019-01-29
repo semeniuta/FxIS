2f5237e89a8dd1b54081d5d56a41ad4aabf4f29a
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2019-01-29 14:49:26 +0100

    ExtendedImageStreamRequester is now header-only

--- include/ImageStreamRequester.h
+++ include/ImageStreamRequester.h
@@ -32,7 +32,28 @@ void extendedImageRequestThread(
         ResT& processing_result,
         ThreadsafeQueue<AsyncImageRequest>& q,
         EventObject& stop_event
-);
+) {
+
+    AsyncImageRequest req;
+
+    while (true) {
+
+        q.pop(req);
+
+        im_stream.getImage(
+                req.timestamp,
+                resp,
+                processing_result
+        );
+
+        req.promise_ptr->set_value(true);
+
+        if (stop_event.hasOccured()) {
+            break;
+        }
+    }
+
+}
 
 
 class BaseImageStreamRequester {
@@ -87,11 +108,29 @@ class ExtendedImageStreamRequester : public BaseImageStreamRequester {
 
 public:
 
-    explicit ExtendedImageStreamRequester(ExtendedImageStream<ResT>& im_stream);
+    explicit ExtendedImageStreamRequester(ExtendedImageStream<ResT>& im_stream) : image_stream(im_stream) { }
 
-    void start() override;
+    void start() override {
 
-    void copyData(ImageResponse& out, ResT& processing_result);
+        thread_ptr = std::unique_ptr<std::thread>(new std::thread(
+                extendedImageRequestThread<ResT>,
+                std::ref(this->image_stream),
+                std::ref(this->image_response),
+                std::ref(this->processing_result),
+                std::ref(this->q_in),
+                std::ref(this->eo_stop)
+        ));
+
+        thread_ptr->detach();
+
+    }
+
+    void copyData(ImageResponse& out_im, ResT& out_proc_res) {
+
+        out_im = this->image_response;
+        out_proc_res = this->processing_result;
+
+    }
 
 private:
 
